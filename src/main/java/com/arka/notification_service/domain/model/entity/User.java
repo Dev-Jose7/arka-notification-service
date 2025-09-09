74d74ce4fcc862f12b766e63410a3f431c4083b8
Jose Navarro <jfnr398@gmail.com>
2025-09-08 22:20:56 -0500

    feat(domain): add phoneNumber field with basic validation to User entity class

--- src/main/java/com/arka/notification_service/domain/model/entity/User.java
+++ src/main/java/com/arka/notification_service/domain/model/entity/User.java
@@ -2,6 +2,7 @@ package com.arka.notification_service.domain.model.entity;
 
 import com.arka.notification_service.domain.model.enums.NotificationChannel;
 import com.arka.notification_service.domain.model.valueobject.Email;
+import com.arka.notification_service.domain.model.valueobject.PhoneNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,23 +14,27 @@ public class User {
     private final UUID id;
     private final String name;
     private final Email email;
+    private final PhoneNumber phoneNumber;
     private final boolean wantsNotifications;
     private final boolean prefersEmail;
     private final boolean prefersWhatsapp;
     private final boolean prefersSms;
     private final boolean prefersPush;
 
-    public User(UUID id, String name, Email email, boolean wantsNotifications,
-                boolean prefersEmail, boolean prefersWhatsapp, boolean prefersSms, boolean prefersPush) {
+    public User(UUID id, String name, Email email, PhoneNumber phoneNumber,
+                boolean wantsNotifications, boolean prefersEmail, boolean prefersWhatsapp,
+                boolean prefersSms, boolean prefersPush) {
 
         if (id == null) throw new IllegalArgumentException("ID cannot be null.");
         if (name == null || name.isBlank())
             throw new IllegalArgumentException("Name cannot be null or blank.");
         if (email == null) throw new IllegalArgumentException("Email cannot be null.");
+        if (phoneNumber == null) throw new IllegalArgumentException("Phone number cannot be null.");
 
         this.id = id;
         this.name = name;
         this.email = email;
+        this.phoneNumber = phoneNumber;
         this.wantsNotifications = wantsNotifications;
         this.prefersEmail = prefersEmail;
         this.prefersWhatsapp = prefersWhatsapp;
@@ -78,6 +83,10 @@ public class User {
 
     public Email getEmail() {
         return this.email;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return this.phoneNumber;
     }
 
     @Override
